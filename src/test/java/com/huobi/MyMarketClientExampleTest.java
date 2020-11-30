4bffffddefef7734f074468b788306acfb6be5ce
wangwei.0822@163.com <wangwei.0822@163.com>
2020-11-30 21:14:38 +0800

    修改测试类

--- src/test/java/com/huobi/MyMarketClientExampleTest.java
+++ src/test/java/com/huobi/MyMarketClientExampleTest.java
@@ -1,4 +1,4 @@
-package com.huobi.examples;
+package com.huobi;
 
 import com.huobi.client.MarketClient;
 import com.huobi.client.req.market.*;
@@ -15,10 +15,8 @@ public class MyMarketClientExampleTest {
 
     public static void main(String[] args) {
 //        newDatas();
-//        historyDatas();
 //        sub();
-//        sub2();
-        sub3();
+//        sub3();
     }
 
     private static void newDatas() {
@@ -36,18 +34,6 @@ public class MyMarketClientExampleTest {
 
         list.forEach(candlestick -> {
             System.out.println(candlestick.toString());
-        });
-    }
-
-    private static void historyDatas() {
-        String symbol = "btcusdt";
-        MarketClient marketClient = MarketClient.create(new HuobiOptions());
-        List<MarketTrade> marketHistoryTradeList = marketClient.getMarketHistoryTrade(MarketHistoryTradeRequest
-                .builder()
-                .symbol(symbol).size(10000)
-                .build());
-        marketHistoryTradeList.forEach(marketTrade -> {
-            System.out.println(marketTrade.toString());
         });
     }
 
@@ -71,38 +57,15 @@ public class MyMarketClientExampleTest {
         });
     }
 
+
     /**
-     * 利用socket订阅方式获取历史数据
+     * 利用socket订阅方式获取历史数据,时间范围
      * @Description TODO
      * @Author wangwei.0822@163.com
      * @Date 2020/11/23 15:09
      * @Param
      * @return
      **/
-    private static void sub2(){
-        String symbol = "btcusdt";
-        MarketClient marketClient = MarketClient.create(new HuobiOptions());
-        marketClient.reqCandlestick(ReqCandlestickRequest.builder()
-                .symbol(symbol)
-                .interval(CandlestickIntervalEnum.MIN15)
-//                .from(1606048200l)
-                .from(1601481600l)//2020-10-01 00:00:00
-//                .to(1606015800l)
-                .to(1604160000l)//2020-11-01 00:00:00
-                .build(), candlestickReq -> {
-
-            System.out.println(candlestickReq.toString());
-            candlestickReq.getCandlestickList().forEach(candlestick -> {
-                System.out.println("candlestick:" + candlestick.toString());
-            });
-        });
-
-//        ReqCandlestickRequest request, ResponseCallback<CandlestickReq> callback
-
-
-
-    }
-
     private static void sub3(){
         String symbol = "btcusdt";
         long from=1604160000;
@@ -110,9 +73,9 @@ public class MyMarketClientExampleTest {
         MarketClient marketClient = MarketClient.create(new HuobiOptions());
         marketClient.reqCandlestick(ReqCandlestickRequest.builder()
                 .symbol(symbol)
-                .interval(CandlestickIntervalEnum.MIN60)//每次最多返回300条,以此类推时间范围为（60m*300）=18000分钟时间范围，超出则报异常
-                .interval(CandlestickIntervalEnum.HOUR4)//每次最多返回300条,以此类推时间范围为（4h*300）=1200小时时间范围，超出则报异常
-                .interval(CandlestickIntervalEnum.DAY1)//每次最多返回300条,以此类推时间范围为（1d*300）=300天时间范围，超出则报异常
+                .interval(CandlestickIntervalEnum.MIN60)//每次最多返回300条,以此类推时间范围为（60m*300）=18000分钟时间范围，超出则报异常from: 1601481600 to: 1604160000, out of range
+                .interval(CandlestickIntervalEnum.HOUR4)//每次最多返回300条,以此类推时间范围为（4h*300）=1200小时时间范围，超出则报异常from: 1601481600 to: 1604160000, out of range
+                .interval(CandlestickIntervalEnum.DAY1)//每次最多返回300条,以此类推时间范围为（1d*300）=300天时间范围，超出则报异常from: 1601481600 to: 1604160000, out of range
                 .from(from)//2020-10-01 00:00:00
                 .to(to)
                 .build(), candlestickReq -> {
